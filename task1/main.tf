00d74a829458a9a307cf1516df5d5f9a87118847
vic <116874558+victoriatushnytska@users.noreply.github.com>
2026-04-05 21:23:25 +0300

    Update main.tf

--- task1/main.tf
+++ task1/main.tf
@@ -41,15 +41,14 @@ resource "digitalocean_firewall" "main" {
   }
 }
 
+resource "digitalocean_ssh_key" "default" {
+  name       = "exam-ssh-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCttIlhehwaH7vpe+ZRX77vY7arNPzMNAY0w9m5OkfLP8RVqlJnIFZ63RaNHd55uJEw8nrQwpVf6J+bXLd784GknHppSbLJl7UZW9uWdY5mREgiLtnCAHUi8S1gzfut1eTg+9fKbC+wmwnbUJEd/YNPRLND7sf2QI08U+FdxTHFhqAY7WwiYVm8vTXx949zOrfj8/FN+ZB3lgV55WOAtIjfPBNMcNavDek/aDnYY9PG5fKI6VKrKc7pj2EIU52zjwAdvdi00iVmhFgU032KaSkId1Jjw8MVo17BSkf4eEghPjeAqsYBaMsbdIinvvpVOHzHfPHpz4Ywi0pNKce0q1srVvGH0pIZLZzU6LaRz/EFKZ+Y2l77Hu/6ZmSregIH7rMQxRmWTLsvjECfo8639doX2mWnQEoQNUtxxJt8Wrd2H0cDX88mVVk52AvU9e0XW73TQfxbGJo9KT++PNNQYtMp89PEJX7MizvQj1hlV+E1EvHYHVGjdd4ztabFAIWeG7kGXAJz8gZnxJTZQXrBT4GpacGfA6BPTBSHYWEaFBS0S+F2ru1UhEDQyohHs4iKxC22nTw1C8mbGoIpbBiu4B3taIQy34yEvtkgrpA/2liusTk+fJViCsdKeY3aT2m568rkidTOFXWCOsIt6vNMO/h8wHs3cgUnsWi1jZvNd0c8CQ== vikatushnytska@vikas-MacBook-Air.local"
+}
+
 resource "digitalocean_droplet" "node" {
   name     = "${var.surname}-node"
   size     = "s-2vcpu-4gb"
   image    = "ubuntu-24-04-x64"
   region   = var.region
-  vpc_uuid = digitalocean_vpc.main.id
-}
-
-resource "digitalocean_spaces_bucket" "main" {
-  name   = "${var.surname}-bucket"
-  region = var.region
-}
+  vpc_
