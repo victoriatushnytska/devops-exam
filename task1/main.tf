7ba6ff00ff5d3304c394710e6bdd79b87c166947
vic <116874558+victoriatushnytska@users.noreply.github.com>
2026-04-05 21:04:22 +0300

    Create main.tf

---
+++ task1/main.tf
@@ -0,0 +1,55 @@
+resource "digitalocean_vpc" "main" {
+  name     = "${var.surname}-vpc"
+  region   = var.region
+  ip_range = "10.10.10.0/24"
+}
+
+resource "digitalocean_firewall" "main" {
+  name = "${var.surname}-firewall"
+  droplet_ids = [digitalocean_droplet.node.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "8000-8003"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+resource "digitalocean_droplet" "node" {
+  name     = "${var.surname}-node"
+  size     = "s-2vcpu-4gb"
+  image    = "ubuntu-24-04-x64"
+  region   = var.region
+  vpc_uuid = digitalocean_vpc.main.id
+}
+
+resource "digitalocean_spaces_bucket" "main" {
+  name   = "${var.surname}-bucket"
+  region = var.region
+}
