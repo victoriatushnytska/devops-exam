193216be57e769d15a76202c17aadf0c9ac96129
vic <116874558+victoriatushnytska@users.noreply.github.com>
2026-04-05 21:14:00 +0300

    Update provider.tf

--- task1/provider.tf
+++ task1/provider.tf
@@ -6,12 +6,14 @@ terraform {
     }
   }
   backend "s3" {
-    endpoint                    = "fra1.digitaloceanspaces.com"
+    endpoints                   = { s3 = "https://fra1.digitaloceanspaces.com" }
     region                      = "us-east-1"
     key                         = "terraform.tfstate"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
 }
 
